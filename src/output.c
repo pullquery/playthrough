e9fd522a791eb36cce3c3b6ff12a70b0c7402427
pullquery <pullquery@gmail.com>
2024-02-25 19:02:09 +0900

    Debugging!

--- src/output.c
+++ src/output.c
@@ -36,9 +36,9 @@ void reset() {
 void modeOutput(Output o) {
     char* mode = "REPEAT";
 
-    // for (int i = 0; i < o.width - strlen(mode) - 1; ++i) {
-    //     printf(" ");
-    // }
+    for (int i = 0; i < o.width - strlen(mode) - 1; ++i) {
+        printf(" ");
+    }
 
     printf("%s ", mode);
 }
@@ -46,43 +46,44 @@ void modeOutput(Output o) {
 void playingOutput(Output o) {
     char* playing = "sample.opus";
 
-    // for (int i = 0; i < o.width - strlen(playing) - 1; ++i) {
-    //     printf(" ");
-    // }
+    for (int i = 0; i < o.width - strlen(playing) - 1; ++i) {
+        printf(" ");
+    }
 
     printf("%s ", playing);
 }
 
 void directoryOutput(Output o, char** list, int size, int selected) {
-    // printf("\033[2J");
-    // printf("\033[H");
+    printf("\033[2J");
+    printf("\033[H");
 
     modeOutput(o);
 
-    // for (int i = 0; i < size; ++i) {
-    //     if (i == selected) {
-    //         printf(">");
-    //     } else {
-    //         printf(" ");
-    //     }
+    for (int i = 0; i < size; ++i) {
+        if (i == selected) {
+            printf(">");
+        } else {
+            printf(" ");
+        }
 
+        if (strncmp(list[i], ".", 1) == 0) {
+            beginGrey();
+        }
 
-    //     if (strncmp(list[i], ".", 1) == 0) {
-    //         beginGrey();
-    //     }
+        printf("%s", list[i]);
 
-    //     printf("%s", list[i]);
+        if (isDirectory(list[i]) == 1) {
+            beginGrey();
+            printf("/");
+            reset();
+        }
 
-    //     if (isDirectory(list[i]) == 1) {
-    //         beginGrey();
-    //         printf("/");
-    //         reset();
-    //     }
+        printf("\n");
 
-    //     printf("\n");
-
-    //     reset();
-    // }
+        reset();
+    }
 
     playingOutput(o);
+
+    fflush(stdout);
 }
