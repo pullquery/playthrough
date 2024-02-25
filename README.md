dba7f721e0f2b327b5764435ed6f84920346e349
pullquery <pullquery@gmail.com>
2024-02-25 17:32:29 +0900

    ANSI code

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 - Error handling in main logic
 - Optimize main loop
 - Audio control and free logic
-- Apply ANSI escape sequence
+- initSDL, initAudio
+- Remember pointer position
+- ~~Apply ANSI escape sequence~~
--- src/output.c
+++ src/output.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <termios.h>
 
 #include <sys/ioctl.h>
 
 #include "output.h"
+
+#include "directory.h"
 
 void initOutput(Output* o) {
     struct winsize win;
@@ -18,6 +21,18 @@ void initOutput(Output* o) {
     o->height = (int) win.ws_row;
 }
 
+void beginGrey() {
+    printf("\033[90m");
+}
+
+void beginBold() {
+    printf("\033[1m");
+}
+
+void reset() {
+    printf("\033[0m");
+}
+
 void directoryOutput(Output o, char** list, int size, int selected) {
     printf("\n");
 
@@ -28,7 +43,22 @@ void directoryOutput(Output o, char** list, int size, int selected) {
             printf(" ");
         }
 
-        printf("%s\n", list[i]);
+
+        if (strncmp(list[i], ".", 1) == 0) {
+            beginGrey();
+        }
+
+        printf("%s", list[i]);
+
+        if (isDirectory(list[i]) == 1) {
+            beginGrey();
+            printf("/");
+            reset();
+        }
+
+        printf("\n");
+
+        reset();
     }
 
     for (int i = 0; i < o.height - size - 2; ++i) {
